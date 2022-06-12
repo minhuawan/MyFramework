c43ec4a168e023dff27f94518d4390fe459f8fe9
minhuawan <minhua.wan17@gmail.com>
2022-06-13 00:14:55 +0800

    unity 屏幕适配真狗屎

---
+++ Assets/MyFramework/Runtime/Services/UI/FuckA.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace PSDtoUI
+{
+    /// <summary>
+    ///     Screen.safeAreaが返す値をシミュレーションする
+    ///     1. `Default`の場合
+    ///         セーフエリアの矩形がパディングなしとして解釈される
+    ///     2. それ以外の場合
+    ///         実機確認時に`Screen.safeArea`で取得できる値を`SizeAttribute`に記載する
+    /// </summary>
+    public enum SafeAreaSimulationType
+    {
+        Default = 0,
+
+        [Size("iPhone X/XS 2436x1125 Landscape", 132f, 63f, 2172f, 1062f)]
+        iPhoneXLandscape,
+        [Size("Xiaomi RedMi Note7 pro 2260x1080 Landscape", 80, 0, 2260, 1080)]
+        XiaomiNote7,
+    }
+
+    /// <summary>
+    ///     セーフエリアの矩形情報を保持するための属性
+    /// </summary>
+    public class SizeAttribute : Attribute
+    {
+        public string Name;
+        public Rect SafeArea;
+
+        public SizeAttribute(string name, float x, float y, float width, float height)
+        {
+            Name = name;
+            SafeArea = new Rect(x, y, width, height);
+        }
+    }
+
+    /// <summary>
+    ///     `SafeAreaSimulationType`からセーフエリアを取得するための拡張メソッド
+    /// </summary>
+    public static class SafeAreaSimulationTypeExtension
+    {
+        public static string Name(this SafeAreaSimulationType simulationType)
+        {
+            var value = simulationType.ToString();
+            var attribute = simulationType.GetType()
+                .GetMember(value)
+                .First()
+                .GetCustomAttribute<SizeAttribute>();
+            return attribute.Name;
+        }
+
+        public static Rect SafeArea(this SafeAreaSimulationType simulationType)
+        {
+            var value = simulationType.ToString();
+            var attribute = simulationType.GetType()
+                .GetMember(value)
+                .First()
+                .GetCustomAttribute<SizeAttribute>();
+            return attribute.SafeArea;
+        }
+    }
+}
---
+++ Assets/MyFramework/Runtime/Services/UI/SafeArea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyFramework.Services.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SafeArea : MonoBehaviour
+    {
+        public bool stretchHorizontally;
+        public bool stretchVertically;
+
+        RectTransform _rectTransform;
+        DeviceOrientation _previousOrientation;
+
+        private void OnEnable()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _previousOrientation = Input.deviceOrientation;
+            ApplySafeArea();
+        }
+
+        private void Update()
+        {
+            if (_previousOrientation != Input.deviceOrientation)
+            {
+                _previousOrientation = Input.deviceOrientation;
+                ApplySafeArea();
+            }
+        }
+
+        public void ApplySafeArea()
+        {
+            var safeAreaSize = Screen.safeArea;
+            if (stretchHorizontally)
+            {
+                safeAreaSize.x = 0f;
+                safeAreaSize.width = Screen.width;
+            }
+
+            if (stretchVertically)
+            {
+                safeAreaSize.y = 0f;
+                safeAreaSize.height = Screen.height;
+            }
+
+            _rectTransform.anchorMin = new Vector2(0f, 0f);
+            _rectTransform.anchorMax = new Vector2(1f, 1f);
+            _rectTransform.anchoredPosition = safeAreaSize.center - new Vector2(Screen.width / 2f, Screen.height / 2f);
+            _rectTransform.sizeDelta =
+                new Vector2(safeAreaSize.width - Screen.width, safeAreaSize.height - Screen.height);
+        }
+    }
+}
