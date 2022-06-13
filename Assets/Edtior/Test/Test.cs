163b9ff7e75ba516374c3fe059f4d0afcbed3658
minhuawan <minhua.wan17@gmail.com>
2022-06-14 02:14:44 +0800

    update node

--- Assets/Edtior/Test/Test.cs
+++ Assets/Edtior/Test/Test.cs
@@ -1,73 +1,114 @@
-// #if UNITY_EDITOR
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using System.IO;
-// using System.Threading;
-// using System.Threading.Tasks;
-// using UnityEditor;
-// using UnityEngine;
-//
-// public class Test : MonoBehaviour
-// {
-//     void Awake()
-//     {
-//         TaskScheduler.UnobservedTaskException += (sender, args) =>
-//         {
-//             Debug.LogError("UnobservedTaskException " + args.Exception);
-//         };
-//         Debug.LogError("thread id " + Thread.CurrentThread.ManagedThreadId);
-//         Fun1();
-//         Fun1();
-//     }
-//
-//     async void Fun1()
-//     {
-//         // return
-//         var task = Task.Run(async () =>
-//         {
-//             Debug.LogError("thread id " + Thread.CurrentThread.ManagedThreadId);
-//             // Fun2();
-//             throw new Exception("Fun1");
-//         });
-//         task.GetAwaiter().OnCompleted(() =>
-//         {
-//             Debug.LogError("im over");
-//         });
-//     }
-//     
-//     
-//     async void Fun2()
-//     {
-//         await Task.Run(async () =>
-//         {
-//             Debug.LogError("thread id " + Thread.CurrentThread.ManagedThreadId);
-//             Thread.Sleep(1000);
-//             throw new Exception("Fun2");
-//             return 1;
-//         });
-//     }
-//     
-//     async Task<int> Fun3()
-//     {
-//         await Task.Run(async () =>
-//         {
-//             Debug.LogError("thread id " + Thread.CurrentThread.ManagedThreadId);
-//             Thread.Sleep(1000);
-//             throw new Exception("Fun3");
-//         });
-//         return 1;
-//     }
-//
-//     IEnumerator TestCoroutine()
-//     {
-//         int i = 0;
-//         while (i++ < 4)
-//         {
-//             Debug.LogError("tick " + i);
-//             yield return new WaitForSeconds(1);
-//         }
-//         Debug.LogError("finished");
-//     }
-// }
-// #endif
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MyFramework.Services.Network.HTTP;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Text text;
+
+    [SerializeField] private Button button;
+
+    async void Start()
+    {
+        var dict = new Dictionary<string, Action>();
+
+        // await Delay().ConfigureAwait(false);
+
+        dict["VoidTask"] = VoidTask;
+        dict["TaskFun"] = () => TaskFun();
+        dict["Http"] = () => Http();
+        dict["ClickButton"] = () => ClickButton();
+
+        var parent = button.transform.parent;
+        foreach (var keyValuePair in dict)
+        {
+            var clone = GameObject.Instantiate(button, parent);
+            clone.transform.localScale = Vector3.one;
+            clone.GetComponentInChildren<Text>().text = keyValuePair.Key;
+            clone.onClick.AddListener(() => { keyValuePair.Value.Invoke(); });
+        }
+
+        button.gameObject.SetActive(false);
+    }
+
+    async Task Delay()
+    {
+        await Task.Delay(1000);
+    }
+
+    void Update()
+    {
+        text.text = Time.frameCount.ToString();
+    }
+
+    private async void VoidTask()
+    {
+        await Task.Run(async () =>
+        {
+            await Task.Delay(3000);
+            Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+            throw new Exception("On VoidTask exception");
+        });
+    }
+
+
+    private async Task TaskFun()
+    {
+        var task = new Task(async () =>
+        {
+            await Task.Delay(1000);
+            Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Run(async () =>
+            {
+                Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+                await Task.Delay(1000);
+                Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+                throw new Exception("On TaskFun internal exception");
+            });
+            throw new Exception("On TaskFun exception");
+        });
+        task.UnWrap();
+        task.ContinueWith(task => { Debug.LogError("final ?"); });
+        task.Start();
+    }
+
+    private async Task Http()
+    {
+        Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+        var request = UnityWebRequest.Get("http://www.baidu.com");
+        // var task = Task.Run(async () =>
+        // {
+        //     
+        // }).UnWrap();
+        Debug.LogError("thread id: " + Thread.CurrentThread.ManagedThreadId);
+        var connection = new HttpConnection(request);
+        var result = await connection.Connect();
+        Debug.LogError("response data: \n" + request.downloadHandler.text);
+    }
+
+    private async Task ClickButton()
+    {
+        var buttons = FindObjectsOfType<Button>();
+        var tasks = buttons.Select(PressButton);
+        Task<Button> finishedButton = await Task.WhenAny(tasks);
+        Debug.LogError("finished clicked " + finishedButton.Result.GetComponentInChildren<Text>().text);
+    }
+
+    async Task<Button> PressButton(Button button)
+    {
+        bool isPress = false;
+        button.onClick.AddListener(() => isPress = true);
+        while (!isPress)
+        {
+            await Task.Yield();
+        }
+
+        return button;
+    }
+}
---
+++ Assets/Edtior/Test/TestTaskExtenstion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class TestTaskExtenstion
+{
+    public static Task UnWrap(this Task task)
+    {
+        if (task.IsCompleted)
+        {
+            return task;
+        }
+
+        if (task.Exception != null)
+        {
+            Debug.LogError("task exception " + task.Exception);
+            return task;
+        }
+
+        task.ContinueWith(t =>
+        {
+            if (t.Exception != null)
+            {
+                var token = DateTime.Now.ToString();
+                for (var i = 0; i < t.Exception.InnerExceptions.Count; i++)
+                {
+                    Debug.LogError($"{token} {i} / {t.Exception.InnerExceptions.Count} task exception on continue with " + t.Exception.InnerExceptions[i]);
+                }
+            }
+        });
+        return task;
+    }
+}
