bbb91e2fdc3edfcec3831ab6a16ec7786e957a1e
minhuawan <minhua.wan17@gmail.com>
2022-07-18 23:48:52 +0800

    lua binder

---
+++ Assets/MyFramework/Runtime/Services/Lua/LuaViewBinder.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+#endif
+
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using MyFramework.Runtime.Services.UI;
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace MyFramework.Runtime.Services.Lua
+{
+    public class LuaViewBinder : MonoBehaviour
+    {
+        private List<GameObject> keys;
+        private List<Type> values;
+    }
+
+    [CustomEditor(typeof(LuaViewBinder))]
+    public class LuaViewBinderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Refresh"))
+            {
+                Refresh();
+            }
+        }
+
+        private void Refresh()
+        {
+        }
+    }
+}
